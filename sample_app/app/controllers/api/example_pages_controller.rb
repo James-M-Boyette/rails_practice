4b175ca04263bcac6d947f5b030c9090a82effff
James Boyette <james.boyette@gmail.com>
2021-02-28 10:47:19 -0500

    Test sub-app Inclusion

--- sample_app/app/controllers/api/example_pages_controller.rb
+++ sample_app/app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,9 @@
 class Api::ExamplePagesController < ApplicationController
+  def hello_action
+    render json: { message: "hello" }
+    # This defines the web-response, or what will be sent back when hello_action is requested; you can render json, html, or even xml
+  end
+  def test_action
+    render json: {message: "test"}
+  end
 end
--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/hello_path" => "example_pages#hello_action"
+    # the "/hello_path" is what you'll enter into your url
+    # After your hash-rocket, the first part ("example_pages")points to the name of your controller
+    # The second part ("hello_action") points to a method *in* that controller ... methods, in Rails, are known as "actions"
+    get "/test_this" => "example_pages#test_action"
+  end
 end
