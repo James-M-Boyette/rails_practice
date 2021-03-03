79146f95c57538f85174240811b786bdb1245e2f
James Boyette <james.boyette@gmail.com>
2021-03-03 18:19:52 -0500

    Add Lotto Numbers feature

--- fortune_app/app/controllers/api/pages_controller.rb
+++ fortune_app/app/controllers/api/pages_controller.rb
@@ -14,7 +14,13 @@ class Api::PagesController < ApplicationController
   # 2. The page should return 6 randomly generated numbers between 1 and 60 so as to predict the upcoming lotto numbers.
 
   def lotto_number
-    render json: "lotto_number"
+    # render json: "lotto_number"
+    numbers = (1..60).to_a
+    @winning_numbers = []
+    6.times do
+      @winning_numbers << numbers.sample
+    end
+    render "lotto_number.json.jb"
   end
 
   # 3. Every time a user visits/refreshes the page, the page should tell the user the number of visits made to that page so far. NOTE: This is impossible, but you should try your hand at this for a few minutes to discover why it’s impossible.
