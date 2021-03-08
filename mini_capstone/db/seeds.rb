d8cb5066dd7f3c194f2a7051731fa8d085bd5750
James Boyette <james.boyette@gmail.com>
2021-03-08 13:56:40 -0500

    Add Products DB

--- mini_capstone/db/seeds.rb
+++ mini_capstone/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+products_without_supplier_ID = Product.where(supplier_id: nil)
+products_without_supplier_ID.each do |product|
+  random_supplier_id = Supplier.all.sample.id
+  # or
+  # product.supplier_id = [1,2,3].sample
+  product.supplier_id = random_supplier_id
+  product.save!
+end
+
+# add quantity to products
+
+# put in Model product.rb
+# def supplier 
+  # Supplier.find_by(id: supplier_id)
+# end
