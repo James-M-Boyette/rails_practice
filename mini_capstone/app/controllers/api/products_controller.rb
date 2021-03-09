32b347dee1957eba7d5565f285e58164492bfd93
James Boyette <james.boyette@gmail.com>
2021-03-09 14:52:36 -0500

    Add Image DB table, Implement pw security

---
+++ mini_capstone/app/controllers/api/products_controller.rb
@@ -0,0 +1,11 @@
+class ProductsController < ApplicationController
+  # if current_user
+  #   @products = Product.all
+  # else
+  #   @products = []
+  # end
+  # p "HERE IS THE CURRENT USER"
+  # p current_user
+  # p "THERE WAS TEH CURRENT USER"
+  # render "index.json.jb"
+end
--- mini_capstone/db/schema.rb
+++ mini_capstone/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_171220) do
+ActiveRecord::Schema.define(version: 2021_03_09_023138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "url"
+    t.integer "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "inventories", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -32,6 +39,14 @@ ActiveRecord::Schema.define(version: 2021_03_07_171220) do
     t.string "name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
