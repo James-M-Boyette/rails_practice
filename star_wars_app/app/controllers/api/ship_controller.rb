98b8121640e651581025895b0b5ed5288d449d3c
James Boyette <james.boyette@gmail.com>
2021-03-16 20:30:59 -0400

    Add HTML 3rd Party API integration & key obfuscation

--- star_wars_app/app/controllers/api/ship_controller.rb
+++ star_wars_app/app/controllers/api/ship_controller.rb
@@ -1,2 +1,14 @@
 class Api::ShipController < ApplicationController
+  def index
+    response = HTTP.get("https://swapi.dev/api/starships/?search=falcon")
+    render json: response.parse
+    @employees = HTTP
+      .headers({
+        "X-User-Email" => Rails.application.credentials.aws[:email]}, 
+        "Authorization" => "Bearer #{Rails.application.credentials.news_api[:api_key]}"
+      })
+      .get("http://localhost:3000/api/v1/employees")
+      .parse
+    render "index.html.erb"
+  end
 end
